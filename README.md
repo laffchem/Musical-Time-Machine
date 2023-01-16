6020d300bd9f6b0f58dc1913e22ba80706cf068a
John Laffey <laffeychem@gmail.com>
2023-01-15 20:42:19 -0500

    Added error handling and better instructions to get it to work

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Musical Time Machine
 
-***
+---
 
 ### Instructions
 
-This program is pretty simple. To get it to work, you need a spotify client id, client secret, & redirect uri. 
+This program is pretty simple. To get it to work, you need to register a developer app and get the following stuff.
+
+1. client_id
+2. client_secret
+3. redirect_uri (note you can use example.com or something because you have to just paste in whatever spotify opens in your browser.)
+
+Either store them as environment variables or in your script as strings.
 
 Then all you need to do is follow the input prompts and enter the data.
 
-This will then make a playlist on your spotify account. 
-
-### Need to add
-
-I need to add error handling for proper date input, will do later. This was mainly a proof of concept project for a class I took.
+This will then make a playlist on your spotify account for specific dates in time!
--- main.py
+++ main.py
@@ -9,56 +9,75 @@ import datetime as dt
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 REDIRECT_URI = os.environ.get('REDIRECT')
+
 print(logo)
 
-print("Welcome to the Musical Time Machine, this creates a playlist from the Billboard Top 100 Playlist from a given date!")
+print(
+  "Welcome to the Musical Time Machine, this creates a playlist from the Billboard Top 100 Playlist from a given date!"
+)
 
-time_travel = input("What year would you like to travel to? Type this as YYYY-MM-DD:\n")
+year = None
 
-time_travel_date = dt.datetime.strptime(time_travel, '%Y-%m-%d')
-year = dt.datetime.strftime(time_travel_date, "%Y")
-print(year)
+while year == None:
+  try:
+    time_travel = input(
+      "What year would you like to travel to? Type this as YYYY-MM-DD:\n")
+    time_travel_date = dt.datetime.strptime(time_travel, '%Y-%m-%d')
+  except ValueError as err:
+    print(f"Incorrect input: ", err)
+  else:
+    year = dt.datetime.strftime(time_travel_date, "%Y")
+    print(year)
 
-
-response = requests.get(f"https://www.billboard.com/charts/hot-100/{time_travel}")
+response = requests.get(
+  f"https://www.billboard.com/charts/hot-100/{time_travel}")
 response.raise_for_status()
 
 songs = response.text
 
 soup = BeautifulSoup(songs, 'html.parser')
 
-song_names_spans = soup.find_all("h3", class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+song_names_spans = soup.find_all(
+  "h3",
+  class_=
+  "c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only"
+)
 
-song_list = [song.getText().replace('\n','').replace('\t','') for song in song_names_spans]
+song_list = [
+  song.getText().replace('\n', '').replace('\t', '')
+  for song in song_names_spans
+]
 
 # print(song_list)
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=REDIRECT_URI,
-        scope="playlist-modify-public",
-        show_dialog=True,
-        cache_path="token.txt",
-    )
-)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+  client_id=CLIENT_ID,
+  client_secret=CLIENT_SECRET,
+  redirect_uri=REDIRECT_URI,
+  scope="playlist-modify-public",
+  show_dialog=True,
+  cache_path="token.txt",
+))
 user_id = sp.current_user()["id"]
 
 # Search for songs that were scraped, then append their spotify uris to a list.
 
 song_uris = []
 for song in song_list:
-    result = sp.search(q=f"track:{song} year:{year}", type='track')
-    print(result)  
-    try:
-        uri = result['tracks']['items'][0]['uri']
-        song_uris.append(uri)
-    except IndexError:
-        print(f"{song} doesn't exist in Spotify. Skipped.")
+  result = sp.search(q=f"track:{song} year:{year}", type='track')
+  print(result)
+  try:
+    uri = result['tracks']['items'][0]['uri']
+    song_uris.append(uri)
+  except IndexError:
+    print(f"{song} doesn't exist in Spotify. Skipped.")
 
 #Create the playlist
-playlist = sp.user_playlist_create(user=user_id, name=f"Billboard Top 100 from {time_travel}",public=True, collaborative=False)
+playlist = sp.user_playlist_create(
+  user=user_id,
+  name=f"Billboard Top 100 from {time_travel}",
+  public=True,
+  collaborative=False)
 playlist_id = playlist['id']
 
 sp.playlist_add_items(playlist_id=playlist_id, items=song_uris)
